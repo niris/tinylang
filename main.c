b065492eed53d4d7ae299697e11c80db8b8924a3
yne <yne@users.noreply.github.com>
2018-08-13 20:18:58 +0200

    can store and print a given character

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 void main(){
+	char storage = '?'; // must be outside the while !
 	while(1){
 		char c = getchar();
 		if(c==EOF)break;
 		switch(c){
 			case ' ':case '\n':case '\r': break;//skip spaces
-			case 'h':printf("Hi !\n");break;
+			case 's':storage=getchar();break;
+			case 'p':printf("storage=%c\n", storage);break;
 			default:printf("unknown char '%c'\n", c);
 		}
 	}
