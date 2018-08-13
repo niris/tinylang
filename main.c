f902bbf7a47d5e450ea47b7120d16b825f01ba7f
yne <yne@users.noreply.github.com>
2018-08-13 20:42:57 +0200

    create + instruction to add 2 indexed number into a storage index

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#define aton(c) (c-'0') // ascii to number
+#define ntoa(c) (c+'0') // number to ascii
+
 void main(){
 	char storage[256]={}; // must be outside the while !
 	while(1){
@@ -6,6 +9,14 @@ void main(){
 		if(c==EOF)break;
 		switch(c){
 			case ' ':case '\n':case '\r': break;//skip spaces
+			case '+':{
+				char src1_index = getchar();
+				char src1_value = aton(storage[src1_index]);
+				char src2_index = getchar();
+				char src2_value = aton(storage[src2_index]);
+				char dest_index = getchar();
+				storage[dest_index] = ntoa(src1_value + src2_value);
+				}break;
 			case 's':{
 				char index = getchar();
 				char value = getchar();
