06cce8f4976a9b97e4df32f131f78ddac49ac96a
yne <yne@users.noreply.github.com>
2018-08-13 20:10:58 +0200

    intepret h letters and say Hi !

--- main.c
+++ main.c
@@ -3,6 +3,9 @@ void main(){
 	while(1){
 		char c = getchar();
 		if(c==EOF)break;
-		printf("c = %c\n", c);
+		switch(c){
+			case 'h':printf("Hi !\n");break;
+			default:printf("unknown char '%c'\n", c);
+		}
 	}
 }
