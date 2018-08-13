1e46194d730b0dafc39e8c9c88b65861d08f0285
yne <yne@users.noreply.github.com>
2018-08-13 20:28:10 +0200

    index based store and print, for more value (s8h s9i p8 p9 p9)

--- main.c
+++ main.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 void main(){
-	char storage = '?'; // must be outside the while !
+	char storage[256]={}; // must be outside the while !
 	while(1){
 		char c = getchar();
 		if(c==EOF)break;
 		switch(c){
 			case ' ':case '\n':case '\r': break;//skip spaces
-			case 's':storage=getchar();break;
-			case 'p':printf("storage=%c\n", storage);break;
+			case 's':{
+				char index = getchar();
+				char value = getchar();
+				storage[index] = value;
+				}break;
+			case 'p':{
+				char index = getchar();
+				printf("storage[%c]=%c\n", index, storage[index]);
+				}break;
 			default:printf("unknown char '%c'\n", c);
 		}
 	}
