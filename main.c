1201dedcf4563562166bd0416df59b3c301e2704
yne <yne@users.noreply.github.com>
2018-08-13 20:52:00 +0200

    read from file instead of stdin

--- main.c
+++ main.c
@@ -4,26 +4,28 @@
 
 void main(){
 	char storage[256]={}; // must be outside the while !
-	while(1){
-		char c = getchar();
+	FILE* file = fopen("code.txt","r");
+	if(!file)printf("unable to open code.txt file");
+	else while(1){
+		char c = getc(file);
 		if(c==EOF)break;
 		switch(c){
 			case ' ':case '\n':case '\r': break;//skip spaces
 			case '+':{
-				char src1_index = getchar();
+				char src1_index = getc(file);
 				char src1_value = aton(storage[src1_index]);
-				char src2_index = getchar();
+				char src2_index = getc(file);
 				char src2_value = aton(storage[src2_index]);
-				char dest_index = getchar();
+				char dest_index = getc(file);
 				storage[dest_index] = ntoa(src1_value + src2_value);
 				}break;
 			case 's':{
-				char index = getchar();
-				char value = getchar();
+				char index = getc(file);
+				char value = getc(file);
 				storage[index] = value;
 				}break;
 			case 'p':{
-				char index = getchar();
+				char index = getc(file);
 				printf("storage[%c]=%c\n", index, storage[index]);
 				}break;
 			default:printf("unknown char '%c'\n", c);
